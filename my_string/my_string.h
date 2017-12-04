e3fe7c019f415de711a63ac2c17969e83b83bb34
deleteLater <mikcczhang@qq.com>
2017-12-04 11:35:39 +0800

    Finish KMP

--- my_string/my_string.h
+++ my_string/my_string.h
@@ -2,10 +2,10 @@
 
 #include <assert.h>
 namespace my_string {
-	#define no_pos -1;	//0xffffffff
+#define no_pos -1;	//0xffffffff
 
-	//can deal with overlapMemory 
-	char* my_memcpy(char* des, const char* src,size_t len) {
+	//can deal with overlapMemory
+	char* my_memcpy(char* des, const char* src, size_t len) {
 		assert(des != nullptr && src != nullptr);
 		char *ret = des;
 		bool overlapMemeryAfterSrc = (des > src && src + len >= des);
@@ -51,35 +51,35 @@ namespace my_string {
 			return (*src == '\0') ? 0 : my_strlen(++str);
 		*/
 	}
-	
+
 	char* my_strcpy(char* des, const char* src) {
 		my_memcpy(des, src, my_strlen(src));
 		return des;
 	}
-	
+
 	char* my_strncpy(char* des, const char* src, size_t len) {
-		my_memcpy(des,src,len);
+		my_memcpy(des, src, len);
 		return des;
 	}
-	
+
 	char* my_strcat(char* des, const char* src) {
 		char* ret = des;
 		while (*des != '\0') {
 			des++;
 		}
-		my_memcpy(des,src,my_strlen(src));
+		my_memcpy(des, src, my_strlen(src));
 		return ret;
 	}
-	
+
 	char* my_strncat(char* des, const char* src, size_t len) {
 		char* ret = des;
 		while (*des != '\0') {
 			des++;
 		}
-		my_memcpy(des,src,len);
+		my_memcpy(des, src, len);
 		return ret;
 	}
-	
+
 	int my_strchr(const char* src, char ch) {
 		const char* sos = src;
 		while (*src != '\0') {
@@ -89,7 +89,7 @@ namespace my_string {
 		}
 		return no_pos;
 	}
-	
+
 	int my_r_strchr(const char* src, char ch) {
 		const char *eos = src + my_strlen(src);
 		while (eos != src) {
@@ -99,11 +99,12 @@ namespace my_string {
 		}
 		return (*eos == ch) ? 0 : no_pos;
 	}
-	
+
 	int my_strmat(const char* txt, char *pat) {
 		int txt_len = my_strlen(txt);
 		int pat_len = my_strlen(pat);
-		int *next = my_space::caculateNext(pat);
+		int *next = new int[pat_len];
+		my_space::caculateNext(pat, next);	//init nextArray
 		int src_ptr = 0;
 		int pat_ptr = 0;
 		while (src_ptr < txt_len && pat_ptr < pat_len) {
@@ -122,10 +123,21 @@ namespace my_string {
 }
 
 namespace my_space {
-	int* caculateNext(char *pat) {
+	void* caculateNext(char *pat, int next[]) {
 		int pat_len = my_strlen(pat);
-		int *ret = new int[pat_len] {};
+		next[0] = -1;
 
-		return ret;
+		int k = -1;
+		int pat_ptr = 0;
+		while (pat_ptr < pat_len - 1) {
+			if (k == -1 || next[pat_ptr] == next[k]) {
+				k++;
+				pat_ptr++;
+				next[pat_ptr] = k;
+			}
+			else {
+				k = next[pat_ptr];
+			}
+		}
 	}
 }
